01264ea2a11d7bc2f95ba1a7b49298ee3f698a0e
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-03-18 17:49:51 +0100

    Added a new menu: "CHANGES" that daily generate from http://github.com/padrino/padrino-framework/raw/master/CHANGES.rdoc our page.

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,3 +1,7 @@
+require 'rdoc/markup/simple_markup'
+require 'rdoc/markup/simple_markup/to_html'
+require 'open-uri'
+
 PadrinoWeb.controllers :base do
 
   get :index, :map => "/" do
@@ -7,5 +11,20 @@ PadrinoWeb.controllers :base do
   get :team, :map => "/team" do
     @team = Account.all
     render 'base/team'
+  end
+
+  get :changes, :map => "/changes" do
+    changes = Padrino.root("tmp/changelog-#{Time.now.strftime("%d%m%Y")}")
+    unless File.exist?(changes)
+      markup = SM::SimpleMarkup.new
+      formatter = SM::ToHtml.new
+      Dir[Padrino.root("tmp/changelog-*")].each { |f| File.delete(f) }
+      rdoc = open("http://github.com/padrino/padrino-framework/raw/master/CHANGES.rdoc").read
+      rdoc.gsub!(/= CHANGES\n\n/,'') # remove redundant <h1>CHANGES</h1>
+      html = markup.convert(rdoc, formatter)
+      html.sub!(/^<h2>/, '<h2 style="border-top:none">') # remove border from the first h2
+      File.open(changes, "w") { |f| f.write html }
+    end
+    render :haml, "-title 'Changes'\n" + File.read(changes)
   end
 end
