20acb5fad5794a633b7f245033b9be8f6d9144a0
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-03-22 14:13:23 +0100

    Removed debug code.

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -7,7 +7,6 @@ Admin.controllers :base do
   post :sort, :map => "/sort/:id" do
     return unless %w(guide page account).include?(params[:id])
     params[:ids].split(",").each_with_index do |id, position|
-      logger.error "#{id} => #{position}"
       klass = params[:id].classify.constantize
       klass.find(id).update_attributes(:position => position)
     end
