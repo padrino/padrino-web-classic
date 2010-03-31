c551cc726359fc36843dc9d7612c1c3ccea82519
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-03-31 15:26:03 +0200

    Add Subject to exception mails.

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,10 @@
 class PadrinoWeb < Padrino::Application
   ##
   # Exception Notifier
-  # 
+  #
   register ExceptionNotifier
-  set :exceptions_from, "Padrino WebSite <exceptions@padrinorb.com>"
-  set :exceptions_to,   Account.all.map(&:email)
-  set :exceptions_page, "base/errors"
+  set :exceptions_subject "PadrinoWeb"
+  set :exceptions_from,   "Padrino WebSite <exceptions@padrinorb.com>"
+  set :exceptions_to,      Account.all.map(&:email)
+  set :exceptions_page,    "base/errors"
 end # PadrinoWeb
