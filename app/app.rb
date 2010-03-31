6f9f5f08e0066814e44ab209eb0c23ac1f508989
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-03-31 15:29:05 +0200

    Forgot a comma.

--- app/app.rb
+++ app/app.rb
@@ -3,8 +3,8 @@ class PadrinoWeb < Padrino::Application
   # Exception Notifier
   #
   register ExceptionNotifier
-  set :exceptions_subject "PadrinoWeb"
-  set :exceptions_from,   "Padrino WebSite <exceptions@padrinorb.com>"
+  set :exceptions_subject, "PadrinoWeb"
+  set :exceptions_from,    "Padrino WebSite <exceptions@padrinorb.com>"
   set :exceptions_to,      Account.all.map(&:email)
   set :exceptions_page,    "base/errors"
 end # PadrinoWeb
