d4d6151c54867bfa393dc228ae4ed3cdf5071d72
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-06-12 10:40:02 +0200

    Another try

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -26,11 +26,11 @@ PadrinoWeb.controllers :base do
       Dir[Padrino.root("tmp/changelog-*")].each { |f| File.delete(f) }
       rdoc = open("http://github.com/padrino/padrino-framework/raw/master/CHANGES.rdoc").read
       rdoc.gsub!(/= CHANGES\n\n/,'') # remove redundant <h1>CHANGES</h1>
-      rdoc.gsub!(/"/, "'") # convert " in '
       html = markup.convert(rdoc, formatter)
       html.sub!(/^<h2>/, '<h2 style="border-top:none">') # remove border from the first h2
+      html.gsub!(/^/, "  ") # add two spaces before each line aka haml indentation
       File.open(changes, "w") { |f| f.write html }
     end
-    render :haml, "- title 'Changes'\n=\"#{File.read(changes)}\""
+    render :haml, "- title 'Changes'\n:plain\n#{File.read(changes)}"
   end
 end
