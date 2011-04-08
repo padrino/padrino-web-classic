0bd0522a78c5da3600c7f66e070521ae942da0d7
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-04-08 22:34:13 +0200

    Added session_secret and admin/posts index refactoring.

--- admin/app.rb
+++ admin/app.rb
@@ -2,6 +2,8 @@ class Admin < Padrino::Application
   register Padrino::Helpers
   register Padrino::Mailer
   register Padrino::Admin::AccessControl
+
+  set :session_secret, "7043bc560e73c46f0d5eabedbabd217f9f5277e6935047bb9430296ab7b47a44"
   set :delivery_method, :smtp => {
     :address         => 'smtp.lipsiasoft.com',
     :port            => '25',
